c3b58d58ac0b336e9102cf9b28f531978043ab3f
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-01-31 17:13:10 +0900

    Fix parse_attrs method

--- lib/haml2slim/converter.rb
+++ lib/haml2slim/converter.rb
@@ -28,7 +28,7 @@ module Haml2Slim
           case line[0]
             when ?%, ?., ?# then parse_tag(line)
             when ?:         then "#{line[1..-1]}:"
-            when ?!         then line == "!!!" ? line.sub(/^!!!/, 'doctype html') : line.sub(/^!!!/, 'doctype') 
+            when ?!         then line == "!!!" ? line.sub(/^!!!/, 'doctype html') : line.sub(/^!!!/, 'doctype')
             when ?-, ?=     then line
             when ?~         then line.sub(/^~/, '=')
             when ?/         then line.sub(/^\//, '/!')
@@ -58,28 +58,38 @@ module Haml2Slim
       end
     end
 
-    def parse_attrs(attrs, key_prefix='')
+    def parse_attrs(attrs, key_prefix="")
       data_temp = {}
-      attrs.gsub!(/:data\s*=>\s*\{([^\}]*)\}/) do
-        key = rand(99999).to_s
-        data_temp[key] = parse_attrs($1, 'data-')
-        ":#{key} => #{key}"
-      end
+
       [
-        /,?( ?):?"?([^"'{ ]+)"?\s*=>\s*([^,]*)/, 
-        /,?( ?)"?([^"'{ ]+)"?:\s*([^,]*)/
+        /data:\s*\{\s*([^\}]*)\s*\}/, # { a: b } (Ruby 1.7 Hash Syntax)
+        /:data\s*=>\s*\{([^\}]*)\s*\}/, # { a => b }
+      ].each do |regexp|
+        attrs.gsub!(regexp) do
+          key = rand(99999).to_s
+          data_temp[key] = parse_attrs($1, "data-")
+          ":#{key} => #{key}"
+        end
+      end
+
+      [
+        /,?( ?):?"?([^"'{ ]+)"?:\s*(:?[^,]*)/, # { a: b } (Ruby 1.7 Hash Syntax)
+        /,?( ?):?"?([^"'{ ]+)"?\s*=>\s*(:?[^,]*)/, # { a => b }
       ].each do |regexp|
         attrs.gsub!(regexp) do
           space = $1
           key = $2
-          value = $3
+          value = $3.strip
           wrapped_value = value.to_s =~ /\s+/ ? "(#{value})" : value
+          wrapped_value = wrapped_value.start_with?(":") ? "\"#{wrapped_value.delete(':')}\"" : wrapped_value
           "#{space}#{key_prefix}#{key}=#{wrapped_value}"
         end
       end
+
       data_temp.each do |k, v|
         attrs.gsub!("#{k}=#{k}", v)
       end
+
       attrs
     end
   end
